b056a472b307a463376810e360fba69d6d912e0d
Yachi Lo <yaachi@gmail.com>
2019-02-15 15:06:12 +0800

    🐛 fix

--- conf.d/nix.fish
+++ conf.d/nix.fish
@@ -1,5 +1,3 @@
-function nix
-  if test -e "~/.nix-profile/etc/profile.d/nix.sh"
-    eval (bash -c "source ~/.nix-profile/etc/profile.d/nix.sh; echo export NIX_PATH=\"\$NIX_PATH\"; echo export PATH=\"\$PATH\"")
-  end
+if test -e "~/.nix-profile/etc/profile.d/nix.sh"
+  eval (bash -c "source ~/.nix-profile/etc/profile.d/nix.sh; echo export NIX_PATH=\"\$NIX_PATH\"; echo export PATH=\"\$PATH\"")
 end
